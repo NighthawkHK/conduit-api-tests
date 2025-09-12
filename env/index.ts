a75c6f4c500f6ac6d771e0bf76bfe925e046b530
Maksym Vovk <wolfram.vovk@gmail.com>
2025-09-12 19:18:39 +0300

    Set default API URL in environment configuration

--- env/index.ts
+++ env/index.ts
@@ -4,7 +4,7 @@ const env = cleanEnv(process.env, {
   USER_EMAIL: email(),
   USER_PASSWORD: str(),
   USER_USERNAME: str(),
-  API_URL: url(),
+  API_URL: url({ default: 'https://conduit-api.learnwebdriverio.com' }),
 });
 
 export default env;
