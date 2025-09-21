8a377304a5ffb4d00b48bc0ab23d94fa85b87124
Maksym Vovk <wolfram.vovk@gmail.com>
2025-09-21 21:08:40 +0300

    Update yml file

--- env/index.ts
+++ env/index.ts
@@ -1,9 +1,9 @@
 import { cleanEnv, str, url, email } from 'envalid';
 
 const env = cleanEnv(process.env, {
-  USER_EMAIL: email(),
-  USER_PASSWORD: str(),
-  USER_USERNAME: str(),
+  USER_EMAIL: email({ desc: 'Email for a test user' }),
+  USER_PASSWORD: str({ desc: 'Password for a test user' }),
+  USER_USERNAME: str({ desc: 'Username for a test user' }),
   API_URL: url({ default: 'https://conduit-api.learnwebdriverio.com' }),
 });
 
